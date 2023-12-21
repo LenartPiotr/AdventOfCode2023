4b7650c46469f773e3b28b56e8f0e9eb07f2019a
Piotr3pl <plenart13@gmail.com>
2023-12-21 12:14:04 +0100

    Add part1 to day 21

--- AdventOfCode2023/Table.h
+++ AdventOfCode2023/Table.h
@@ -10,9 +10,39 @@ public:
 	int width;
 	int height;
 
-	Table(int w, int h, T def);
-	~Table();
+	Table(int w, int h, T def) {
+		this->tab = new T * [w];
+		for (int x = 0; x < w; x++) {
+			this->tab[x] = new T[h];
+			for (int y = 0; y < h; y++) this->tab[x][y] = def;
+		}
+		this->width = w;
+		this->height = h;
+	}
+	~Table() {
+		for (int x = 0; x < this->width; x++) delete[] this->tab[x];
+		delete[] this->tab;
+	}
 
-	//template<typename T2>
-	//friend std::ostream& operator<<(std::ostream& os, const Table<T2>& t);
+	inline bool inRange(int x, int y) {
+		return x >= 0 && x < this->width && y >= 0 && y < this->height;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Table<T>& t) {
+		for (int y = 0; y < t.height; y++) {
+			for (int x = 0; x < t.width; x++) {
+				if (x != 0) os << " ";
+				os << t.tab[x][y];
+			}
+			os << std::endl;
+		}
+		return os;
+	}
+	T* operator[](int rowIndex) {
+		return tab[rowIndex];
+	}
+
+	T& operator()(int rowIndex, int colIndex) {
+		return tab[rowIndex][colIndex];
+	}
 };
--- AdventOfCode2023/day21.cpp
+++ AdventOfCode2023/day21.cpp
@@ -3,11 +3,41 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <queue>
 
 #include "days.h"
 #include "Table.h"
 
 using namespace std;
+
+struct Step {
+	int x;
+	int y;
+	int dist;
+};
+
+void part1(Table<bool> t, int sx, int sy, int limit) {
+	Table<int> visited(t.width, t.height, INT_MAX);
+	
+	queue<Step> steps;
+	steps.push({ sx, sy, 0 });
+
+	pair<int, int> directions[4] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+	while (!steps.empty()) {
+		Step s = steps.front();
+		steps.pop();
+		if (s.dist > limit) continue;
+		if (!t.inRange(s.x, s.y)) continue;
+		if (visited[s.x][s.y] <= s.dist) continue;
+		visited[s.x][s.y] = s.dist;
+		for (int i = 0; i < 4; i++) {
+			steps.push({ s.x + directions[i].first, s.y + directions[i].second, s.dist + 1 });
+		}
+	}
+
+	cout << visited << endl;
+}
 
 void day21() {
 	vector<string> lines = readLinesFromFile("./data/test.txt");
@@ -19,7 +49,7 @@ void day21() {
 	for (int y = 0; y < height; y++) {
 		string line = lines[y];
 		for (int x = 0; x < width; x++) {
-			if (line[x] == '#') tab.tab[x][y] = true;
+			if (line[x] == '#') tab[x][y] = true;
 			else if (line[x] == 'S') {
 				sx = x;
 				sy = y;
@@ -27,5 +57,5 @@ void day21() {
 		}
 	}
 
-	// cout << tab << endl;
+	part1(tab, sx, sy, 6);
 }
